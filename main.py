8ec84eabeac9c0a50e0e581e72d58e8d57bc5f1c
mvizcainog <0215141@up.edu.mx>
2024-12-12 13:18:25 -0600

    Subir aplicación Streamlit

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ import io
 import pandas as pd
 
 # Título de la aplicación
-st.title("Análisis de Datos de Centro Agosto 24")
+st.title("Análisis de Delitos en la ZMG")
+st.subheader("Por Miguel Vizcaíno")
 
 # Descripción
-st.write("Este es un ejemplo de cómo descargar un archivo ZIP, extraerlo y visualizar los primeros registros de un archivo CSV dentro del ZIP.")
+st.write("Empezamos descargando el Data Set de la página oficial del IIEE (Instituto de Información Estadística y Geográfica)")
+st.write("Fuente: https://iieg.gob.mx/ns/wp-content/uploads/2024/09/Centro_agosto24.zip")
+st.write("Se hace una limpieza de los datos, resultando en el siguinte Data Set")
 
 # URL del archivo ZIP
 url = "https://iieg.gob.mx/ns/wp-content/uploads/2024/09/Centro_agosto24.zip"
@@ -21,11 +24,11 @@ if response.status_code == 200:
     # Abrir el archivo ZIP en memoria
     with zipfile.ZipFile(io.BytesIO(response.content)) as z:
         # Listar los archivos dentro del ZIP
-        st.write("Archivos dentro del ZIP:", z.namelist())
+        #st.write("Archivos dentro del ZIP:", z.namelist())
 
         # Tomar el primer archivo CSV dentro del ZIP
         csv_filename = z.namelist()[0]
-        st.write(f"Abrir archivo CSV: {csv_filename}")
+        #st.write(f"Abrir archivo CSV: {csv_filename}")
 
         # Leer el archivo CSV en un DataFrame de pandas
         with z.open(csv_filename) as csv_file:
@@ -36,3 +39,4 @@ if response.status_code == 200:
         st.dataframe(df.head())  # Muestra el DataFrame de forma interactiva en la página
 else:
     st.error(f"Error al descargar el archivo ZIP: {response.status_code}")
+
