8a874c12d7bb3c0b082c510ac51f8d8cabb2b421
mvizcainog <0215141@up.edu.mx>
2024-12-12 21:07:46 -0600

    Subir aplicación Streamlit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 import requests
 import zipfile
 import io
@@ -15,8 +16,8 @@ st.subheader("Por Miguel Vizcaíno")
 #------------------------ INTRODUCCIÓN --------------------------------
 
 # Descripción
-st.write("Empezamos descargando el Data Set de la página oficial del IIEE (Instituto de Información Estadística y Geográfica)")
-st.write("Fuente: https://iieg.gob.mx/ns/wp-content/uploads/2024/09/Centro_agosto24.zip")
+st.write("Empezamos descargando el Dataset de cada zona de Jalisco de la página oficial del IIEE (Instituto de Información Estadística y Geográfica)")
+st.write("Fuente: https://iieg.gob.mx/ns/?page_id=31143")
 st.write("El Data Set seleccionado presenta los siguientes problemas:")
 st.write("  - No todas las colonias de cada delito están registradas")
 st.write("  - No todas las ubicaciones de cada delito están registradas")
@@ -80,6 +81,12 @@ df_hour['hora_valida'] = pd.to_datetime(df_hour['hora'], format='%H:%M', errors=
 counts = df_hour['hora_valida'].value_counts()
 # Mantener solo las filas válidas y eliminar la columna auxiliar
 df_hour = df_hour[df_hour['hora_valida']].drop(columns=['hora_valida'])
+# Se asegura que la columna fecha tenga formato datatime
+df_hour['fecha'] = pd.to_datetime(df_hour['fecha'])
+# Crear la columna 'weekday' con el nombre del día de la semana
+df_hour['weekday'] = df_hour['fecha'].dt.day_name()
+# Crear la columna 'hora_interval' extrayendo solo la hora
+df_hour['hora_interval'] = df_hour['hora'].str.split(':').str[0].astype(int)
 
 # --- DataFrame - Loc
 # Hacemos una copia de df para tener un dataframe con datos limpios de colonia
@@ -150,32 +157,26 @@ st.dataframe(data_summary.style.set_properties(**{
 st.subheader("2. Análisis de Tipos de Bien Afectado")
 # Crear una lista de municipios únicos y agregar la opción "Total"
 municipios = ["Total"] + sorted(df['municipio'].unique())  # Ordenar alfabéticamente y agregar "Total"
-
 # Crear un menú desplegable para seleccionar el municipio
 selected_municipio_bien = st.selectbox(
     "Selecciona un municipio:",
     municipios,
     key="selectbox_municipio_bienes"
 )
-
 # Filtrar los datos según el municipio seleccionado
 if selected_municipio_bien == "Total":
     filtered_df = df  # Usar todos los registros
 else:
     filtered_df = df[df['municipio'] == selected_municipio_bien]
-
 # Calcular el número de semanas entre la primera y última fecha
 filtered_df['fecha'] = pd.to_datetime(filtered_df['fecha'])
 num_semanas = ((filtered_df['fecha'].max() - filtered_df['fecha'].min()).days // 7) + 1
-
 # Agrupar por 'bien_afectado' y contar las ocurrencias
 bienes_count = filtered_df['bien_afectado'].value_counts().reset_index()
 bienes_count.columns = ['bien_afectado', 'count']  # Renombrar columnas para claridad
-
 # Calcular el porcentaje y el conteo semanal
 bienes_count['percentage'] = (bienes_count['count'] / bienes_count['count'].sum()) * 100
 bienes_count['count_week'] = bienes_count['count'] / num_semanas
-
 # Crear la gráfica de pie con Plotly
 fig = px.pie(
     bienes_count,
@@ -185,10 +186,8 @@ fig = px.pie(
     labels={'bien_afectado': 'Bien Afectado', 'percentage': 'Porcentaje'},
     hover_data=['count'],  # Mostrar el conteo al pasar el mouse
 )
-
 # Mostrar la gráfica de pie en Streamlit
 st.plotly_chart(fig)
-
 # Crear la gráfica de barras horizontales por conteo semanal
 barras_fig = px.bar(
     bienes_count.sort_values(by='count_week', ascending=False),
@@ -199,10 +198,8 @@ barras_fig = px.bar(
     orientation='h',
     color='bien_afectado'
 )
-
 # Mostrar la gráfica de barras en Streamlit
 st.plotly_chart(barras_fig)
-
 # Mostrar la tabla resumida centrada
 data_summary = bienes_count[['bien_afectado', 'count', 'count_week', 'percentage']]
 st.subheader("Datos resumidos")
@@ -211,3 +208,36 @@ st.dataframe(data_summary.style.set_properties(**{
 }).set_table_styles([
     dict(selector='th', props=[('text-align', 'center')])
 ]))
+
+# --------------------------- TIEMPO -----------------------------------
+# Agrupar los datos por 'weekday' y 'hora_interval' y contar la cantidad de delitos
+heatmap_data = df_hour.groupby(['weekday', 'hora_interval']).size().reset_index(name='conteo')
+
+# Crear el heatmap usando Plotly Express
+fig = px.density_heatmap(heatmap_data,
+                         x="weekday", 
+                         y="hora_interval",
+                         nbinsy=12,
+                         z="conteo", 
+                         color_continuous_scale="reds",  # Paleta de colores rojos
+                         title="Distribución de Delitos por Día de la Semana y Hora",
+                         labels={"conteo": "Número de Delitos", "hora_interval": "Hora", "weekday": "Día de la Semana"})
+
+# Ajustar el orden de los días de la semana
+fig.update_layout(
+    yaxis={'title': 'Hora del Día', 'dtick': 2},  # Usar tick cada hora
+    xaxis={
+        'title': 'Día de la Semana',
+        'tickmode': 'array',
+        'tickvals': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+        'ticktext': ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'],
+        'categoryorder': 'array',  # Forzar el orden específico
+        'categoryarray': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']  # Orden de los días
+    }
+)
+
+# Título de la aplicación
+st.title("Distribución de Delitos por Día de la Semana y Hora")
+
+# Mostrar el gráfico interactivo en Streamlit
+st.plotly_chart(fig)
